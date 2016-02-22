865ab9de114e8c585ec3b3d10977abb9726d4b0c
Johnnylkc <momozzz1231@gmail.com>
2016-02-22 22:30:41 +0800

    Initial Commit

---
+++ JohnnyGram/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JohnnyGram
+//
+//  Created by 劉坤昶 on 2016/2/22.
+//  Copyright © 2016年 JohnnyKetchup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
