64ed26ebca234cf719bcfe6a1eea279b667e5aaa
Johnnylkc <momozzz1231@gmail.com>
2016-02-23 13:09:39 +0800

    02/23 13:10

--- JohnnyGram/SignUpVC.swift
+++ JohnnyGram/SignUpVC.swift
@@ -9,13 +9,53 @@
 import UIKit
 
 class SignUpVC: UIViewController {
+    
+    @IBOutlet weak var avaImage: UIImageView!
+    
+    @IBOutlet weak var userNameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var repeatPassordTextField: UITextField!
+    @IBOutlet weak var fullNameTextField: UITextField!
+    @IBOutlet weak var bioTextField: UITextField!
+    @IBOutlet weak var webTextField: UITextField!
 
-    override func viewDidLoad() {
+    
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak var cancelButton: UIButton!
+    
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    var scrollViewHeight : CGFloat = 0
+    
+    var keyBoard = CGRect()
+    
+    
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func signUp(sender: AnyObject)
+    {
+        
+    }
+    
+    
+    @IBAction func cancel(sender: AnyObject)
+    {
+        
+    }
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
