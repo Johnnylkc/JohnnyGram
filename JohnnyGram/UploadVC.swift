f7dfa22aeccd4b1a419a09c585b2809e9929488a
Johnnylkc <momozzz1231@gmail.com>
2016-03-29 00:27:14 +0800

    329 0030

---
+++ JohnnyGram/UploadVC.swift
@@ -0,0 +1,41 @@
+//
+//  UploadVC.swift
+//  JohnnyGram
+//
+//  Created by 劉坤昶 on 2016/3/28.
+//  Copyright © 2016年 JohnnyKetchup. All rights reserved.
+//
+
+import UIKit
+
+class UploadVC: UIViewController {
+
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+
+    
+    
+    
+    
+    
+    }
+
+//    override func didReceiveMemoryWarning() {
+//        super.didReceiveMemoryWarning()
+//        // Dispose of any resources that can be recreated.
+//    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
